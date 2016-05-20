b2b1453eaef522cfe3434a1d9e448ce40c393f27
Nigel Small <nigel.small@neotechnology.com>
2016-05-20 12:09:41 +0100

    Working handshake

--- src/github.com/nigelsmall/go2neo/bolt/socketclient.go
+++ src/github.com/nigelsmall/go2neo/bolt/socketclient.go
@@ -5,12 +5,36 @@ import (
   "net"
 )
 
-func ConnectTCP() {
-  _, tcperr := net.Dial("tcp", "127.0.0.1:7687") // replace _ with variable, e.g. "conn" once used
+var preamble []byte = []byte{0x60, 0x60, 0xB0, 0x17}
+var handshakeRequest []byte = []byte{0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+                                     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 
-  if tcperr != nil {
-    fmt.Println("Cannot listen to server: ", tcperr)
-  }
+type client struct {
+    readBuffer []byte
 }
 
-// introduce handshake here
+func ConnectTCP() {
+  c := client{readBuffer: make([]byte, 65536)}
+
+  conn, error := net.Dial("tcp", "127.0.0.1:7687")
+  if error != nil {
+    fmt.Println("Cannot listen to server: ", error)
+    return
+  }
+
+  // perform handshake
+  conn.Write(preamble)
+  conn.Write(handshakeRequest)
+  size, error := conn.Read(c.readBuffer)
+  if error != nil {
+    fmt.Println("Cannot receive data: ", error)
+    return
+  }
+  if size != 4 {
+    fmt.Println("Not enough data: ", size)
+    return
+  }
+
+  fmt.Println("Handshake response: ", c.readBuffer[:4])
+
+}
